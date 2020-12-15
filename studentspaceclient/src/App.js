8812061ae6d44d9690afa2feca47a41751580b68
sahil2712 <sahilnaik2712@gmail.com>
2020-12-15 12:22:25 +0530

    Trying just to work

--- studentspaceclient/src/App.js
+++ studentspaceclient/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
        		StudentSpace - An e-portal for all academic information
         </p>
