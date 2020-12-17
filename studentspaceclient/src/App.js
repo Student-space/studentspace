ab3d27f93fa4b179e8c91ff657f29dd531d82e24
Amol Telang <telangamol0909@gmail.com>
2020-12-17 12:56:22 +0530

    added design

--- studentspaceclient/src/App.js
+++ studentspaceclient/src/App.js
@@ -1,12 +1,42 @@
+//importing packages
+import React,{Fragment} from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 
+
+
+
+//importing styles
 import './App.css';
 import './styles/index.css';
-import Navbar from './components/Navbar';
+
+
+
+//importing components
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+
+//main functional component
 function App() {
   return (
-    <div>
-    <Navbar/>
-    </div>
+    <Router>
+      <Fragment>
+
+       {/* Navbar component */}
+        <Navbar/>
+
+        {/* Landin page component */}
+        <Route exact path="/" component={Landing}/>
+
+        {/* switch */}
+        <Switch>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path ="/login" component={Login}/>
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ studentspaceclient/src/components/auth/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import loginIcon from '../../images/loginIcon.png';
+
+function Login() {
+    return (
+        
+           <h1>Login</h1>
+
+        
+    )
+}
+
+
+
+export default Login
+
